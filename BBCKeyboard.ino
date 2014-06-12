1fdc7493cfe8661a1498808ac5a24509fbc9fc61
Andrew Ward <ajw@gbh-warda-mac.local>
2014-06-12 09:42:26 +0100

    leonardo/micro changes

--- BBCKeyboard.ino
+++ BBCKeyboard.ino
@@ -1,29 +1,54 @@
-//#define DEBUG
+// Debug flag
+#define DEBUG
 
 #include "BBCKeyboard.h"
 
+// Output types
+#define OT_SERIAL 1
+#define OT_KEYBOARD 2
+
+#if defined(__AVR_ATmega32U4__)
+// Arduino leonardo / micro - USB built in - use keyboard library to report keys
+# define OUTPUTTYPE OT_KEYBOARD
+
+#elif defined(__AVR_ATmega328p__)
+// Arduino Uno - USB not built in. Output is always serial - an 8 byte report if DEBUG not set for the keyboard firmware
+# define OUTPUTTYPE OT_SERIAL
+
+#else
+# error "Unrecognised mcu type"
+
+#endif
+
+// BBC connector pinout and arduino equivalent
+// +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+// | 0v |RST |1mhz|KBEN|PA4 |PA5 |PA6 |PA0 |PA1 |PA2 |PA3 |PA7 |LED3|CA2 | 5v |LED1|LED2|
+// +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+// |GND |D2  |D3  |GND |D4  |D5  |D6  |D7  |D8  |D9  |D10 |D11 |A2  |D12 | 5V |A1  |A0  |
+// +----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+----+
+
 // Pin numbers
 
-const int led1Pin = A0;
-const int led2Pin = A1;
+const int led1Pin = A1;
+const int led2Pin = A0;
 const int led3Pin = A2;
 
 // Column
-const int pa0Pin = 8;
-const int pa1Pin = 9;
-const int pa2Pin = 10;
-const int pa3Pin = 11;
+const int pa0Pin = 7;
+const int pa1Pin = 8;
+const int pa2Pin = 9;
+const int pa3Pin = 10;
 
 // Row
 const int pa4Pin = 4;
 const int pa5Pin = 5;
 const int pa6Pin = 6;
 
+const int pa7Pin = 11;
 const int ca2Pin = 12;
-const int pa7Pin = 7;
-const int breakPin = 13;
+const int breakPin = 2;
 
-const int clkPin =  2;
+const int clkPin =  3;
 
 // Keyboard state
 char KeyState[MAXCOL][MAXROW];
@@ -35,8 +60,28 @@ unsigned char UsbReport[8];
 
 void setup()
 {
-  // Initialise serial
+  // Set up Serial
+#if OUTPUTTYPE == OT_SERIAL
+  // Initialise serial for report / debug output
   Serial.begin(9600);
+
+#elif OUTPUTTYPE == OT_KEYBOARD
+  // Initialise the keyboard output
+  Keyboard.begin();
+  
+# ifdef DEBUG
+  // Initialise serial and wait if debugging active
+  Serial.begin(9600);
+  while(!Serial);
+
+  Serial.print("KEYBOARD output\n");
+# endif
+
+#else
+  // unknown output type
+# error "UNKNOWN OUTPUTTYPE!"
+
+#endif
 
   // Set up the pins
   pinMode(led1Pin, OUTPUT);
@@ -78,6 +123,7 @@ void setup()
   }
 
 #ifdef DEBUG
+  // Debug initialisation checks
   InitCheck();
 #endif
 }
@@ -95,12 +141,18 @@ void loop()
   digitalWrite(led3Pin, (ctr & 0x400) ? 0 : 1);
   ++ctr;
 
+  // Check each column in turn
   for(Col = 0; Col < 10; Col++){
     SetRowCol(0, Col);
+    
+    // If CA2 pin raised then a key on this column is pressed
     if(digitalRead(ca2Pin)){
       // There is a key down in this column
       for(Row = 1; Row < 8; Row++){
+        // Check each key in this column
         SetRowCol(Row, Col);
+
+        // If PA7 pin raised then this key is down
         if(digitalRead(pa7Pin)){
           // This key is down
           SetKeyState(Col, Row, 'D');
@@ -122,6 +174,8 @@ void loop()
   // Check Row 0
   for(Col = 0; Col < 10; Col++){
     SetRowCol(0, Col);
+
+    // If PA7 pin raised then this key is down
     if(digitalRead(pa7Pin)){
       // This key is down
       SetKeyState(Col, 0, 'D');
@@ -143,6 +197,8 @@ void loop()
 
 void SetRowCol(unsigned int Row, unsigned int Col)
 {
+  // Clock the keyboard array row and column on
+
   digitalWrite(clkPin, LOW);
 
   SetRow(Row);
@@ -153,6 +209,8 @@ void SetRowCol(unsigned int Row, unsigned int Col)
 
 void SetRow(unsigned int Row)
 {
+  // Set current keyboard array row
+  
   digitalWrite(pa4Pin, (Row & 0x1 ? HIGH : LOW));
   digitalWrite(pa5Pin, (Row & 0x2 ? HIGH : LOW));
   digitalWrite(pa6Pin, (Row & 0x4 ? HIGH : LOW));
@@ -160,6 +218,8 @@ void SetRow(unsigned int Row)
 
 void SetCol(unsigned int Col)
 {
+  // Set current keyboard array column
+  
   digitalWrite(pa0Pin, (Col & 0x1 ? HIGH : LOW));
   digitalWrite(pa1Pin, (Col & 0x2 ? HIGH : LOW));
   digitalWrite(pa2Pin, (Col & 0x4 ? HIGH : LOW));
@@ -168,25 +228,26 @@ void SetCol(unsigned int Col)
 
 void SetKeyState(unsigned int Col, unsigned int Row, char State)
 {
+  // Set a key to a given state
+  
   KeyDet *Key;
 
   if(KeyState[Col][Row] != State){
+    
     // Key state changed
     KeyState[Col][Row] = State;
 
     // Get key details
     Key = &KeyDetails[Row][Col];
 
-    // Update the report
-    UpdateReport(Key, State);
-
 #ifdef DEBUG
-    Serial.write(KeyDetails[Row][Col].Desc);
-    Serial.write(" (Row "); 
-    Serial.print(Row); 
-    Serial.write(" Col "); 
+    Serial.write("Key ");
+    Serial.write(Key->Desc);
+    Serial.write(" (Col ");
     Serial.print(Col);
-    Serial.print(") ");
+    Serial.write(" Row ");
+    Serial.print(Row);
+    Serial.write(") ");
     switch(State){
     case 'D':
       Serial.write("down");
@@ -195,14 +256,18 @@ void SetKeyState(unsigned int Col, unsigned int Row, char State)
       Serial.write("up");
       break;
     }      
-    Serial.print("\n");
+    Serial.write("\n");
 #endif
 
+    // Update key
+    UpdateReport(Key, State);
   }
 }
 
 void SetBreakState(char State)
 {
+  // Set the BREAK key state
+  
   KeyDet *Key;
 
   if(BreakState != State){
@@ -212,26 +277,28 @@ void SetBreakState(char State)
     // Get key details
     Key = &BreakDetails;
 
-    // Update the report
-    UpdateReport(Key, State);
-
 #ifdef DEBUG
-    Serial.write("Break"); 
+    Serial.write("Break ");
     switch(State){
     case 'D':
-      Serial.write(" down\n");
+      Serial.write("down");
       break;
     case 'U':
-      Serial.write(" up\n");
+      Serial.write("up");
       break;
     }      
+    Serial.write("\n");
 #endif
 
+    // Update key
+    UpdateReport(Key, State);
   }
 }
 
 void UpdateReport(KeyDet *Key, unsigned char State)
 {
+  // Update the report with a given key and state
+  
   unsigned int Report;
   int SendReport;
 
@@ -245,16 +312,22 @@ void UpdateReport(KeyDet *Key, unsigned char State)
 
     // Send if required
     if(SendReport){
-#ifdef DEBUG
-      Serial.print("REPORT: ");
-      for(int Loop = 0; Loop < 8; Loop++){
-        Serial.print(UsbReport[Loop], HEX);
-        Serial.print(" ");
-      }
-      Serial.print("\n");
-#else
+
+#if OUTPUTTYPE == OT_SERIAL
+      // Use serial output
       Serial.write(UsbReport, 8);
+      
+#elif OUTPUTTYPE == OT_KEYBOARD
+      // Use keyboard HID output
+      HID_SendReport(2, UsbReport, 8);
+
+#endif
+
       // TODO GET LED STATE BACK?
+
+#ifdef DEBUG
+      // Dump the report
+      DumpReport();
 #endif
     }
   }
@@ -262,6 +335,8 @@ void UpdateReport(KeyDet *Key, unsigned char State)
 
 int AddReport(unsigned int Report)
 {
+  // Add a key to the report
+  
   int Changed = 0;
   unsigned char Byte;
 
@@ -289,6 +364,8 @@ int AddReport(unsigned int Report)
 
 int RemoveReport(unsigned int Report)
 {
+  // Remove a key from the report
+  
   int Changed = 0;
   unsigned char Byte;
 
@@ -314,12 +391,25 @@ int RemoveReport(unsigned int Report)
   return Changed;
 }
 
+
 #ifdef DEBUG
 
-// Sanity checks
+void DumpReport()
+{
+  // Dump current key report
+  
+  Serial.print("REPORT: ");
+  for(int Loop = 0; Loop < 8; Loop++){
+    Serial.print(UsbReport[Loop], HEX);
+    Serial.print(" ");
+  }
+  Serial.print("\n");
+}
 
 void InitCheck()
 {
+  // Sanity checks
+  
   unsigned int Col1, Row1;
   unsigned int Col2, Row2;
   unsigned int Elem;
